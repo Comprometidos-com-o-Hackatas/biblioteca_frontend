2013aaa75b9aeb8b863a75e1a8ecb645f647ddeb
Gabz047 <gabe1612mail@gmail.com>
2024-08-14 08:01:43 -0300

    FEAT: Created Login Page and added vue icons

---
+++ src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<template>
+    <main class="main-login">
+        <section class="section-login">
+            <h1>Login</h1>
+
+            <div class="container-login">
+            <div class="login-input-boxes">
+                <div class="input-box">
+                    <p>Username or Email</p>
+                    <input type="text">
+                </div>
+                <div class="input-box">
+                    <p>Password</p>
+                    <input type="text">
+                </div>
+            </div>
+
+            <div class="login-buttons">
+            <button>Next</button>
+            <p>or try</p>
+            <div class="ways-logins-box">
+
+            </div>
+            </div>
+            </div>
+            <span class="mdi mdi-google"></span>
+            <span class="mdi mdi-facebook"></span>
+            <span class="mdi mdi-apple"></span>
+        </section>
+    </main>
+</template>
