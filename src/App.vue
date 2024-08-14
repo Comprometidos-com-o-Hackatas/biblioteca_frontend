8272a11b9f26cb4fb14496b4998b4e46799d333d
Gabz047 <gabe1612mail@gmail.com>
2024-08-14 09:28:04 -0300

    Doing login page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
 <!--Apague isso se for mexer na app-->
-<h1>OLA</h1>
+    
 <RouterView />
 </template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -19,13 +19,41 @@
             <button>Next</button>
             <p>or try</p>
             <div class="ways-logins-box">
-
-            </div>
-            </div>
-            </div>
             <span class="mdi mdi-google"></span>
             <span class="mdi mdi-facebook"></span>
             <span class="mdi mdi-apple"></span>
+            </div>
+            </div>
+            </div>
         </section>
     </main>
 </template>
+<style scoped>
+.section-login {
+    background-image: url('../assets/media/authbackground.png');
+    width: 1150px;
+    height: 100dvh;
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+}
+
+h1 {
+    color: #8A3119;
+    font-size: 100px;
+    font-family: var(--font-poppins);
+    font-weight: 200;
+    position: relative;
+    left: 130px;
+}
+
+.container-login {
+    position: relative;
+    left: 30px;
+    height: 450px;
+    width: 500px;
+    background-color: aquamarine;
+}
+
+</style>
