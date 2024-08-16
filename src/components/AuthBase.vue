4f44ad1f8a4d41f6ed031d9d61806b825929575c
Gabz047 <gabe1612mail@gmail.com>
2024-08-16 00:36:45 -0300

    Created animation and change comp function

---
+++ src/components/AuthBase.vue
@@ -0,0 +1,77 @@
+<script setup>
+import {ref} from 'vue'
+
+const emits = defineEmits([
+    'sign'
+])
+
+const props = defineProps({
+    title: {
+        type: String
+    },
+    first_box: {
+        type: String
+    },
+    second_box: {
+        type: String
+    },
+    third_box: {
+        type: String
+    },
+    hasThird: {
+        type: Boolean,
+        default: false
+    },
+    directiveMessage: {
+        type: String
+    },
+    directiveButton: {
+        type: String
+    },
+    position: {
+        type: String
+    },
+    directivePosition: {
+        type: String
+    }
+ 
+    
+})
+</script>
+<template>
+        <div class="container-login-out" :style="props.directivePosition + ': ' + props.position">
+            <h1>{{props.title}}</h1>
+
+            <div class="container-login">
+            <div class="login-input-boxes">
+                <div class="input-box">
+                    <p>{{ props.first_box }}</p>
+                    <input type="text">
+                </div>
+                <div class="input-box">
+                    <p>{{props.second_box}}</p>
+                    <input type="text">
+                </div>
+                <div class="input-box" v-show="hasThird">
+                    <p>{{props.third_box}}</p>
+                    <input type="text">
+                </div>
+            </div>
+
+            <div class="login-buttons">
+            <button>Next</button>
+            <p>or try</p>
+            <div class="ways-logins-box">
+            <span class="mdi mdi-google mdi-h" ></span>
+            <span class="mdi mdi-facebook"></span>
+            <span class="mdi mdi-apple"></span>
+            </div>
+            <div class="way-sign-box">
+            <p>{{props.directiveMessage}}</p>
+            <span class="sign-text" to="/" @click="emits('sign')" >{{props.directiveButton}}</span>
+            </div>
+            </div>
+            </div>
+        </div>
+       
+</template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,154 +1,20 @@
+<script setup>
+import {ref} from 'vue'
+import AuthBase from '@/components/AuthBase.vue'
+import Sign from '@/components/Sign.vue'
+const changeToSign = ref(true)
+</script>
 <template>
     <main class="main-login">
-        <section class="section-login">
-            <h1>Login</h1>
-
-            <div class="container-login">
-            <div class="login-input-boxes">
-                <div class="input-box">
-                    <p>Username or Email</p>
-                    <input type="text">
-                </div>
-                <div class="input-box">
-                    <p>Password</p>
-                    <input type="text">
-                </div>
-            </div>
-
-            <div class="login-buttons">
-            <button>Next</button>
-            <p>or try</p>
-            <div class="ways-logins-box">
-            <span class="mdi mdi-google mdi-h" ></span>
-            <span class="mdi mdi-facebook"></span>
-            <span class="mdi mdi-apple"></span>
-            </div>
-            <div class="way-sign-box">
-            <p>Dont have an account?</p>
-            <RouterLink to="/">Sign Up</RouterLink>
-            </div>
-            </div>
-            </div>
-        </section>
+        <section class="section-login" :class="{active: !changeToSign}">
+       <AuthBase :title="'Login'" :first_box="'Username or Email'" :second_box="'Password'" :directiveMessage="'Do not have an account?'" :directiveButton="'Sign Up'" :directivePosition="'left'" :position="'204px'" @sign="changeToSign = !changeToSign" v-if="changeToSign == true" />
+       <Sign @sign="changeToSign = !changeToSign" v-else />
+    </section>
     </main>
 </template>
 <style scoped>
 
-.main-login {
-    width: 100%;
-    height: 100dvh;
-    overflow: hidden;
-}
-.section-login {
-    background-image: url('../assets/media/baseTemplate.png');
-    width: 100%;
-    height: 100dvh;
-    background-repeat: no-repeat;
-    background-size: cover;
-    display: flex;
-    flex-direction: column;
-    background-position: left;
-}
-
-h1 {
-    color: #8A3119;
-    font-size: 90px;
-    font-family: var(--font-poppins);
-    font-weight: 200;
-    position: relative;
-    left: 130px;
-}
-
-.container-login {
-    position: relative;
-    left: 30px;
-    height: 300px;
-    width: 500px;
-   margin-top: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-}
-
-.login-input-boxes {
-    width: 100%;
-    height: 50%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
-}
-
-.input-box {
-    width: 100%;
-    & input {
-        width: 100%;
-        height: 35px;
-        font-size: 16px;
-        background-color: transparent;
-        outline: none;
-        border: none;
-        border-bottom: 2px solid #8A3119;
-    }
-}
-
-.input-box p {
-    font-size: 13px;
-    color: #8A3119;
-    font-family: var(--font-poppins);
-    font-weight: 300;
-}
-
-.login-buttons {
-    width: 100%;
-    height: 50%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 10px;
-
-    & button {
-        width: 91px;
-        height: 32px;
-        font-size: 14px;
-        border-radius: 10px;
-        border: none;
-        background-color: #945737;
-        color: white;
-        font-family: var(--font-poppins);
-        margin-top: 15px;
-        cursor: pointer;
-        &:hover {
-        background-color: #79472d;
-        }
-    }
-}
-
-.login-buttons p {
-    font-size: 15px;
-    font-family: var(--font-poppins);
-    color: #8A3119;
-}
-
-.ways-logins-box {
-    width: 100%;
-    display: flex;
-    justify-content: center;    
-    gap: 30px;
-}
-
-.mdi {
-   font-size: 35px;
-   color: black;
-
-   &:hover {
-    color: #8A3119;
-   }
-}
-
-.way-sign-box {
-   display: flex;
-   gap: 10px;
+* {
+    cursor: default;
 }
 </style>
