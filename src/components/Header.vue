055ca1b813367b264154f3c9058630b36f9ce21e
Gabz047 <gabe1612mail@gmail.com>
2024-08-18 22:28:42 -0300

    Done Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,17 @@
+<script setup>
+import {ref} from 'vue'
+
+const data = ref([
+    {icon: 'mdi-home', link: '/login', isActive: false},
+    {icon: 'mdi-magnify', link: '/', isActive: false},
+    {icon: 'mdi-bookmark-outline', link: '#', isActive: false},
+    {icon: 'mdi-book-alert', link: '#', isActive: false},
+    {icon: 'mdi-human-female-female-child', link: '#', isActive: false},
+    {icon: 'mdi-cog', link: '#', isActive: false},
+])
+
+
+</script>
 <template>
     <section class="section-header">
         <div class="container-header">
@@ -6,16 +20,15 @@
                     <img src="../assets/media/crash.webp">
                 </div>
                 <ul>
-                    <li> <span class="mdi mdi-home mdiheader"></span></li>
-                    <li> <span class="mdi mdi-magnify mdiheader"></span></li>
-                    <li> <span class="mdi mdi-bookmark-outline mdiheader"></span></li>
-                    <li> <span class="mdi mdi-facebook mdiheader"></span></li>
-                    <li> <span class="mdi mdi-facebook mdiheader"></span></li>
+                    <li v-for="(item, index) in data" :key="index"> <router-link class="mdiheader-link" :to="item.link" :class="`mdi ${item.icon} mdiheader`" @click="item.isActive = !item.isActive" ></router-link></li>
                 </ul>
+                
             </div>
             <div class="config-content">
-
             </div>
+        </div>
+        <div class="arrow">
+                    <span class="mdi mdi-arrow-right"></span>
         </div>
     </section>
 </template>
