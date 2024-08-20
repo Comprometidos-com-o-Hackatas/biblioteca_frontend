54d4759b3b49ebad2c9a814f375a50c6606e1ad7
Gabz047 <gabe1612mail@gmail.com>
2024-08-19 22:46:32 -0300

    FIX: header css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@ const data = ref([
     {id: 0, icon: 'mdi-home', link: '/login', isActive: false},
     {id: 1, icon: 'mdi-magnify', link: '/', isActive: false},
     {id: 2, icon: 'mdi-bookmark-outline', link: '#', isActive: false},
-    {id: 3, icon: 'mdi-book-alert', link: '#', isActive: false},
+    {id: 3, icon: 'mdi-book-alert', link: '/booked', isActive: false},
     {id: 5, icon: 'mdi-human-female-female-child', link: '#', isActive: false},
     {id: 6, icon: 'mdi-cog', link: '#', isActive: false},
 ])
@@ -23,7 +23,13 @@ function activatedOn(index) {
                     <img src="../assets/media/crash.webp">
                 </div>
                 <ul class="focusicon">
-                    <li v-for="(item, index) in data" :key="index" class="item-header" :class="{active: item.isActive}"> <router-link  :to="item.link" class="mdiheader" :class="`mdi ${item.icon} `"  @click="activatedOn(index)"></router-link></li>
+                 
+                    <li v-for="(item, index) in data" :key="index" class="item-header-box" :class="{active: item.isActive}">
+                    <div class="item-header">
+                    <router-link  :to="item.link" class="mdiheader" :class="`mdi ${item.icon} `"  @click="activatedOn(index)"></router-link>
+                    </div>
+                    </li>
+                    
                 </ul>
                 
             </div>
