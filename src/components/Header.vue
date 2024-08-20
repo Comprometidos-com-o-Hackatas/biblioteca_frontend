c10fd41aaa9d78eb05219d941d9054f881bd8fc3
luansilva25 <luansilva250807@gmail.com>
2024-08-19 21:51:13 -0300

    fix: Fixing header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,19 @@
 <script setup>
-import {ref} from 'vue'
+import {computed, ref} from 'vue'
 
 const data = ref([
-    {icon: 'mdi-home', link: '/login', isActive: false},
-    {icon: 'mdi-magnify', link: '/', isActive: false},
-    {icon: 'mdi-bookmark-outline', link: '#', isActive: false},
-    {icon: 'mdi-book-alert', link: '/booked', isActive: false},
-    {icon: 'mdi-human-female-female-child', link: '#', isActive: false},
-    {icon: 'mdi-cog', link: '#', isActive: false},
+    {id: 0, icon: 'mdi-home', link: '/login', isActive: false},
+    {id: 1, icon: 'mdi-magnify', link: '/', isActive: false},
+    {id: 2, icon: 'mdi-bookmark-outline', link: '#', isActive: false},
+    {id: 3, icon: 'mdi-book-alert', link: '#', isActive: false},
+    {id: 5, icon: 'mdi-human-female-female-child', link: '#', isActive: false},
+    {id: 6, icon: 'mdi-cog', link: '#', isActive: false},
 ])
 
-
+function activatedOn(index) {
+    data.value.forEach(item => item.isActive = false);
+    data.value[index].isActive = true;
+}
 </script>
 <template>
     <section class="section-header">
@@ -19,16 +22,16 @@ const data = ref([
                 <div class="box-photo">
                     <img src="../assets/media/crash.webp">
                 </div>
-                <ul>
-                    <li v-for="(item, index) in data" :key="index"> <router-link class="mdiheader-link" :to="item.link" :class="`mdi ${item.icon} mdiheader`" @click="item.isActive = !item.isActive" ></router-link></li>
+                <ul class="focusicon">
+                    <li v-for="(item, index) in data" :key="index" class="item-header" :class="{active: item.isActive}"> <router-link  :to="item.link" class="mdiheader" :class="`mdi ${item.icon} `"  @click="activatedOn(index)"></router-link></li>
                 </ul>
                 
             </div>
-            <div class="config-content">
+            <div class="config-content" >
             </div>
         </div>
         <div class="arrow">
-                    <span class="mdi mdi-arrow-right"></span>
+        <span class="mdi mdi-arrow-right"></span>
         </div>
     </section>
 </template>
