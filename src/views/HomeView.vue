910d8aedec698910f151e76b222c9f58d45ce820
Gabz047 <gabe1612mail@gmail.com>
2024-08-20 15:03:58 -0300

    Started home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,21 @@
 <script setup>
   import { bookdetails } from '@/utils/book';
   import BookDetail from '@/components/BookDetail.vue';
+  const illes = 4
 </script>
 <template>
-  <main>
-      <div v-for="details in bookdetails" :key="details.id">
-          <div class="card">
-             <router-link :to="'/' + details.id" ><BookDetail :url="details.img" :categories="details.category" :genere="details.genere" is_list="list"/>
-             </router-link>
-            </div>
-      </div>  
+  <main class="main-home">
+    <section class="corners">
+
+  
+      <div class="container-cards" v-for="item in illes">
+          <h3 class="title-container">Mais lidos</h3>
+
+
+          <div class="stand-container">
+            
+          </div>
+      </div>
+    </section>  
   </main>
 </template>
