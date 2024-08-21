5bde65a6aec7806cc20a1146376a9a5e2406cdcc
Gabz047 <gabe1612mail@gmail.com>
2024-08-21 09:26:48 -0300

    Doing home page

--- src/components/BookDetail.vue
+++ src/components/BookDetail.vue
@@ -1,22 +1,36 @@
 <template>
+
   <div :class="is_list != 'list' ? 'detail-container' : 'book-list-container'" :id="is_list != 'list' ? 'detail-container-esp' : null">
+    
     <div :class="is_list != 'list' ? 'book-details' : 'book-list'">
+      
       <div :class="is_list != 'list' ? 'book-details-in' : 'book-list-in'">
+        
         <div :class="is_list != 'list' ? 'box-img' : 'box-list-img'">
           <img :src="url"/>
+          
         </div>
-        <div :class="is_list != 'list' ? 'box-list-container' : 'box-list-list-container'">
-          <div :class="is_list != 'list' ? 'list-container' : 'list-list-container'">
-              <li :id="is_list != 'list' ? 'genere' : 'genere-list'">{{ genere.description }}</li> 
-              <li v-for="category in categories" :key="category.id"  :class="getcategory(category)">{{ category.description }}</li>
-          </div>
-        </div>
+        
+        
+        
       </div>
+      
     </div>
-    <div class="overlay"></div>
+  
     <StarsComp :is_rating="false"  v-show="is_list === 'details'"/>
     <global-button :class="'rating-button'" title="Avaliar" @click="$emit('rate') " v-show="is_list === 'details'"/>
+    <div :class="is_list != 'list' ? 'box-list-container' : 'box-list-list-container'">
+          
+          <div :class="is_list != 'list' ? 'list-container' : 'list-list-container'">
+            
+              <li :id="is_list != 'list' ? 'genere' : 'genere-list'">{{ genere.description }}</li> 
+              <li v-for="category in categories" :key="category.id"  :class="getcategory(category)">{{ category.description }}</li>
+
+          </div>
+        </div>
+        <div class="overlay"></div>
 </div>
+
 </template>
 <script setup>
 import StarsComp from './StarsComp.vue';
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,20 @@
 <script setup>
   import { bookdetails } from '@/utils/book';
   import BookDetail from '@/components/BookDetail.vue';
+import { reactive } from 'vue';
   const illes = 4
   const cards = 10
+
+  const booksList = reactive({
+    mostRead: [],
+    popular: [],
+    launched: [],
+    romance: []
+  })
+
+  function moveRight() {
+
+  }
 </script>
 <template>
   <main class="main-home">
@@ -10,11 +22,18 @@
       <div class="container-cards" v-for="(item, index) in illes" :key="index">
           <h3 class="title-container">Mais lidos</h3>
 
-          <div class="container-book" v-for="(itens, index) in cards" :key="index">
+          <div class="container-book-out">
+          <div class="container-book" v-for="(index, itens) in cards" :key="index">
+
+            <div class="card-container-home" v-for="(details, index) in bookdetails" :key="index">
+             <router-link :to="'/' + details.id" ><BookDetail :url="details.img" :categories="details.category" :genere="details.genere" is_list="list"/>
+             </router-link>
+            </div>
+
             <p>A17</p>
           </div>
-
-          <span class="mdi mdi-arrow-right-thick rarrow"></span>
+          </div>
+          <span class="mdi mdi-arrow-right-thick rarrow" @click="moveRight(index)"></span>
           <div class="stand-container">
             
           </div>
